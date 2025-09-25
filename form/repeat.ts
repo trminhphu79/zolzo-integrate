9db029f853126ddb67ee2136921b439836a122f5
phu tran <phu_tran@epam.com>
2025-09-25 20:12:06 +0700

    update form

--- form/repeat.ts
+++ form/repeat.ts
@@ -1,101 +1,89 @@
-import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormGroup } from '@angular/forms';
+import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
 
-type Address = {
-  street: string;
-  city: string;
-  zip: string;
-};
+type Address = { street: string; city: string };
 
 @Component({
-  selector: 'app-address-form',
-  templateUrl: './address-form.component.html',
+  selector: 'app-address-array',
+  templateUrl: './address-array.component.html',
 })
-export class AddressFormComponent {
-  constructor(private fb: FormBuilder) {
-    // seed with one row
-    this.addAddress();
+export class AddressArrayComponent implements OnInit {
+  form = new FormGroup({ addresses: new FormArray<FormGroup>([]) });
+
+  // model must mirror the FormArray length
+  model: { addresses: Address[] } = { addresses: [{ street: '', city: '' }] };
+
+  // per-row options (keeps field state per item)
+  optionsList: FormlyFormOptions[] = [];
+
+  // fields for ONE address row (keys are relative to the row group)
+  addressFields: FormlyFieldConfig[] = [
+    {
+      fieldGroup: [
+        { key: 'street', type: 'input', templateOptions: { label: 'Street', required: true } },
+        { key: 'city',   type: 'input', templateOptions: { label: 'City',   required: true } },
+      ],
+    },
+  ];
+
+  ngOnInit(): void {
+    this.syncFormArrayWithModel();
   }
 
-  form = this.fb.group({
-    addresses: this.fb.array<FormGroup<Address>>([]),
-  });
-
-  // typed getter for convenience
-  get addresses(): FormArray<FormGroup<Address>> {
-    return this.form.get('addresses') as FormArray<FormGroup<Address>>;
+  get addressesFA(): FormArray<FormGroup> {
+    return this.form.get('addresses') as FormArray<FormGroup>;
   }
 
-  private buildAddressGroup(data?: Partial<Address>): FormGroup<Address> {
-    return this.fb.group<Address>({
-      street: new FormControl(data?.street ?? '', { nonNullable: true, validators: [Validators.required] }),
-      city:   new FormControl(data?.city ?? '',   { nonNullable: true, validators: [Validators.required] }),
-      zip:    new FormControl(data?.zip ?? '',    { nonNullable: true, validators: [Validators.pattern(/^\d{4,6}$/)] }),
-    });
+  // Build/resize the FormArray to match model.addresses
+  private syncFormArrayWithModel() {
+    this.addressesFA.clear();
+    this.optionsList = [];
+    for (let i = 0; i < this.model.addresses.length; i++) {
+      this.addressesFA.push(new FormGroup({}));
+      this.optionsList.push({});
+    }
   }
 
-  addAddress(data?: Partial<Address>) {
-    this.addresses.push(this.buildAddressGroup(data));
+  addRow(initial: Address = { street: '', city: '' }) {
+    this.model.addresses.push(initial);
+    this.addressesFA.push(new FormGroup({}));
+    this.optionsList.push({});
   }
 
-  removeAddress(index: number) {
-    this.addresses.removeAt(index);
+  removeRow(i: number) {
+    this.addressesFA.removeAt(i);
+    this.model.addresses.splice(i, 1);
+    this.optionsList.splice(i, 1);
   }
+
+  trackByIndex = (i: number) => i;
 
   submit() {
-    if (this.form.invalid) {
-      this.form.markAllAsTouched();
-      return;
-    }
-    console.log('✅ Form value:', this.form.value);
+    if (this.form.valid) console.log('model', this.model);
   }
-
-  trackByIndex = (_: number, __: unknown) => _;
 }
 
-
-
 <form [formGroup]="form" (ngSubmit)="submit()">
-  <div formArrayName="addresses">
+  <ng-container formArrayName="addresses">
     <div
-      *ngFor="let group of addresses.controls; let i = index; trackBy: trackByIndex"
+      *ngFor="let group of addressesFA.controls; let i = index; trackBy: trackByIndex"
       [formGroupName]="i"
       class="row"
     >
-      <div>
-        <label>Street</label>
-        <input formControlName="street" />
-        <small class="error" *ngIf="group.get('street')?.touched && group.get('street')?.invalid">
-          Street is required
-        </small>
-      </div>
+      <!-- 👇 Embed Formly for this row -->
+      <formly-form
+        [form]="group"
+        [fields]="addressFields"
+        [model]="model.addresses[i]"
+        [options]="optionsList[i]"
+      ></formly-form>
 
-      <div>
-        <label>City</label>
-        <input formControlName="city" />
-        <small class="error" *ngIf="group.get('city')?.touched && group.get('city')?.invalid">
-          City is required
-        </small>
-      </div>
-
-      <div>
-        <label>ZIP</label>
-        <input formControlName="zip" />
-        <small class="error" *ngIf="group.get('zip')?.touched && group.get('zip')?.invalid">
-          ZIP must be 4–6 digits
-        </small>
-      </div>
-
-      <div class="actions">
-        <button type="button" (click)="removeAddress(i)">Remove</button>
-      </div>
-
+      <button type="button" (click)="removeRow(i)">Remove</button>
       <hr />
     </div>
-  </div>
+  </ng-container>
 
-  <div class="toolbar">
-    <button type="button" (click)="addAddress()">+ Add</button>
-    <button type="submit" [disabled]="form.invalid">Submit</button>
-  </div>
+  <button type="button" (click)="addRow()">+ Add</button>
+  <button type="submit" [disabled]="form.invalid">Submit</button>
 </form>
